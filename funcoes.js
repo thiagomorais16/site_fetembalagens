4facf923dda9643110b4f073d917a6a16a61ab4f
Thiago Morais <118579263+thiagomorais16@users.noreply.github.com>
2023-05-11 10:58:59 -0300

    adicionado funções upgrade no index

--- funcoes.js
+++ funcoes.js
@@ -1,14 +1,35 @@
-let slideIndex = 0;
+var slideIndex = 0;
 showSlides();
 
 function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
+  if (slideIndex > slides.length) { slideIndex = 1 }
+  slides[slideIndex - 1].style.display = "block";
   setTimeout(showSlides, 10000); // muda
+}
+var slideIndex = 1;
+showbotoes(slideIndex);
+
+function plusSlides(n) {
+  showbotoes(slideIndex += n);
+}
+
+function showbotoes(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
 }
